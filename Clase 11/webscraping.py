978838a58e86dcdc3d08b9c72428ae110743e01b
mauriciovilar <vilarmauricio@gmail.com>
2022-10-08 18:45:18 -0300

    Webscraping multipagina y creacion de archivo

--- Clase 11/webscraping.py
+++ Clase 11/webscraping.py
@@ -1,17 +1,34 @@
 import bs4
 import requests
+import pandas as pd
 
-# Obtenemos la busqueda con requests.get
-resultado =  requests.get('https://escueladirecta-blog.blogspot.com/2022/10/el-modulo-secreto-de-python.html')
+# creamos url de las paginas
+url_base = 'http://books.toscrape.com/catalogue/page-{}.html'
 
-#print(resultado.text)
+# Creamos lista vacia para guardar los libros
+lista_libros_top = []
 
-# Hacemos identificacion (pharsing) de elementos con bs4
+# Bucle para itinerar en cada pagina
+for num in range(1, 51):
+    
+    # Obtenenoms Html de la pagina
+    pagina = requests.get(url_base.format(num))
+    
+    # Ordenamos Html
+    sopa = bs4.BeautifulSoup(pagina.text, 'lxml')
 
-sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+    # Obtenemos los datos de los libros que se encuentran dentro de una clase
+    libros = sopa.select('.product_pod')
 
-# Buscamos por nombre de etiqueta, nos trae como lista
-print(sopa.select('title'))
+    # Itineramos en la lista obtenida de datos de los libros
 
-# traemos solo el texto de la lista sin etiquetas
-print(sopa.select('title')[0].getText())
+    for libro in libros:
+
+        # Condicion de Seleccion
+        if len(libro.select('.star-rating.Four')) != 0 or len(libro.select('.star-rating.Five')) != 0:
+            titulo = libro.select('a')[1]['title']
+            precio = libro.select('p')[1].getText()[2:]
+            lista_libros_top.append([titulo, precio]) 
+
+df = pd.DataFrame(lista_libros_top, columns= ['titulo', 'precio'])
+df.to_csv('./libros_top.csv', index= False)

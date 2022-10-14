1d3022d2eb80cf95c35e014062a73940b2409005
mauriciovilar <vilarmauricio@gmail.com>
2022-10-14 17:44:25 -0300

    Colocar nombre proyectos

---
+++ Clase 1 - Creador de Nombres/Primer_Proyecto.py
@@ -0,0 +1,15 @@
+# print("Hola Mundo 'Hola'")
+
+# print('A\tB\tC\tD\tE\tF\tG\tH\tI\tJ\t')
+
+# print('Barra normal: /')
+# print('Barra invertida: \\')Per
+
+# print(input('¿Que estas estudiando?'))
+
+# print('Tu nombre y apellido es: ' + input('Dime tu nombre: ') + ' ' + input('Dime tu Apellido: '))
+
+# Ejercicio del dia
+print('Hola!! Pongamos un nombre a tu Cerveza Artesanal..\n')
+
+print('El nombre de tu Cerveza deberia ser:\n\t"' + input('Dime cual es tu Animal favorito: ') + '-' + input('Dime tu color favorito: ') + '"\n')
---
+++ Clase 8 - Consola de Turnos/practica_pylint.py
@@ -0,0 +1,11 @@
+'''Creamos una funcion suma()'''
+
+
+def sumar(numero1, numero2):
+    '''Explicacion de la funcion'''
+    return numero1+numero2
+
+
+
+SUMA = sumar(5,7)
+print(SUMA)
